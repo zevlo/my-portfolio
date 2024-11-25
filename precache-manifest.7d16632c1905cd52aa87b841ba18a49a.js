74025cedd9f7ce5cd01c2a3f7847a7e0b1409bd5
zevlo <zevlo@users.noreply.github.com>
2024-11-25 00:08:32 +0000

    deploy: 804297f2bf110d390aa1a475704b0319c7a8dc8f

---
+++ precache-manifest.7d16632c1905cd52aa87b841ba18a49a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084e430b9a82e4a115a37a128b09111a",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "3984ba096a70c759e66b",
+    "url": "/my-portfolio/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "ff1a3e516aee22dcc154",
+    "url": "/my-portfolio/static/js/2.67491ddc.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/my-portfolio/static/js/2.67491ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc5a2d6146a3ecaf9da",
+    "url": "/my-portfolio/static/js/3.e642e0d8.chunk.js"
+  },
+  {
+    "revision": "3984ba096a70c759e66b",
+    "url": "/my-portfolio/static/js/main.1844f887.chunk.js"
+  },
+  {
+    "revision": "e8b523f0acff629d2b93",
+    "url": "/my-portfolio/static/js/runtime-main.ac8b017c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-portfolio/static/media/logo.06e73328.svg"
+  }
+]);
